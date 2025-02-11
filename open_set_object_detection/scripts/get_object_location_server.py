44c0ce4af5d59e2a24761a9a121c35efb7481833
Varshith R V <vrv278@gmail.com>
2025-02-12 03:59:01 +0800

    2 rs cameras fully supported

--- open_set_object_detection/scripts/get_object_location_server.py
+++ open_set_object_detection/scripts/get_object_location_server.py
@@ -16,21 +16,30 @@ from torchvision.ops import box_convert
 import tf2_ros, tf2_geometry_msgs
 import bisect
 import os
+
 ####### model parameters threshold ########
 BOX_THRESHOLD = 0.35
 TEXT_THRESHOLD = 0.25
 TEXT_PROMPT = "colored shapes"
-####################################### 
+###########################################
 
+####### camera identifiers ################
+LEFT_CAMERA = "rs_415"
+RIGHT_CAMERA = "rs_435i"
+###########################################
 
 class Deprojection:
 
     def __init__(self) -> None:
         
-        self.depth_image = None # contains the depth images
-        self.camera_info = None # contains the camera info
-        self.color_image = None # contains the color image
-        self.camera_model = image_geometry.PinholeCameraModel()
+        self.left_depth_image = None # contains the left depth images
+        self.left_camera_info = None # contains the left camera info
+        self.left_color_image = None # contains the left color image
+        self.right_depth_image = None # contains the right depth images
+        self.right_camera_info = None # contains the right camera info
+        self.right_color_image = None # contains the right color image
+        self.left_camera_model = image_geometry.PinholeCameraModel()
+        self.right_camera_model = image_geometry.PinholeCameraModel()
         self.cv_bridge = cv_bridge.CvBridge()
         self.latest_result = None
         self.recursion = 0
@@ -38,25 +47,50 @@ class Deprojection:
         self.model = load_model("groundingdino/config/GroundingDINO_SwinT_OGC.py", "weights/groundingdino_swint_ogc.pth")
         rospy.loginfo("Loaded the Grounding Dino model")
 
+        left_camera_color_topic = f"/left/{LEFT_CAMERA}/color/image_raw"
+        left_camera_info_topic = f"/left/{LEFT_CAMERA}/aligned_depth_to_color/camera_info"
+        left_camera_depth_topic = f"/left/{LEFT_CAMERA}/aligned_depth_to_color/image_raw"
+
+        right_camera_color_topic = f"/right/{RIGHT_CAMERA}/color/image_raw"
+        right_camera_info_topic = f"/right/{RIGHT_CAMERA}/aligned_depth_to_color/camera_info"
+        right_camera_depth_topic = f"/right/{RIGHT_CAMERA}/aligned_depth_to_color/image_raw"
+        
         # Subscribers
-        self.depth_image_sub = rospy.Subscriber(
-                                                "/camera/aligned_depth_to_color/image_raw", 
+        self.left_depth_image_sub = rospy.Subscriber(
+                                                left_camera_depth_topic, 
                                                 Image, 
-                                                self.depth_image_callback
+                                                self.left_depth_image_callback
                                                 )
-        self.camera_info_sub = rospy.Subscriber(
-                                                "/camera/aligned_depth_to_color/camera_info", 
+        self.left_camera_info_sub = rospy.Subscriber(
+                                                left_camera_info_topic, 
                                                 CameraInfo, 
-                                                self.camera_info_callback
+                                                self.left_camera_info_callback
                                                 )
-        self.color_image_sub = rospy.Subscriber(
-                                                "/camera/color/image_raw",
+        self.left_color_image_sub = rospy.Subscriber(
+                                                left_camera_color_topic,
                                                 Image,
-                                                self.color_image_callback
+                                                self.left_color_image_callback
+                                                )
+
+        self.right_depth_image_sub = rospy.Subscriber(
+                                                right_camera_depth_topic, 
+                                                Image, 
+                                                self.right_depth_image_callback
+                                                )
+        self.right_camera_info_sub = rospy.Subscriber(
+                                                right_camera_info_topic, 
+                                                CameraInfo, 
+                                                self.right_camera_info_callback
+                                                )
+        self.right_color_image_sub = rospy.Subscriber(
+                                                right_camera_color_topic,
+                                                Image,
+                                                self.right_color_image_callback
                                                 )
         
         # Service
-        server = rospy.Service("get_object_locations", GetObjectLocations, self.get_object_locations)
+        server = rospy.Service("left_get_object_locations", GetObjectLocations, self.left_get_object_locations)
+        server = rospy.Service("right_get_object_locations", GetObjectLocations, self.right_get_object_locations)
         
         # create a publisher to publish the stream
         self.stream_pub = rospy.Publisher("/orange_position", PoseStamped, queue_size=10)
@@ -130,11 +164,10 @@ class Deprojection:
         self.stream_pub.publish(position)
         pass
 
-
-    def get_3d_position(self, x, y):
-        if self.depth_image is None and self.camera_info is None:
+    def get_3d_position(self, x, y, depth_image, camera_info, camera_model):
+        if depth_image is None and camera_info is None:
             return  # Wait until depth image is received
-        depth = (self.depth_image[y, x]/1000)  # Convert to meters
+        depth = (depth_image[y, x]/1000)  # Convert to meters
         if np.isnan(depth) or depth == 0:
             rospy.logwarn("Invalid depth at pixel ({}, {})".format(x, y))
             self.recursion +=1
@@ -145,12 +178,12 @@ class Deprojection:
                 return None
             
         # Project the 2D pixel to 3D point in the camera frame
-        point_3d = self.camera_model.projectPixelTo3dRay((x, y))
+        point_3d = camera_model.projectPixelTo3dRay((x, y))
         print("X multiplier : ",point_3d[0])
         print("Y multiplier : ",point_3d[1])
         point_3d = np.array(point_3d) * depth  # Scale the ray by the depth
         pose = PoseStamped()
-        pose.header.frame_id = self.camera_model.tf_frame
+        pose.header.frame_id = camera_model.tf_frame
         pose.header.stamp = rospy.Time.now()
         pose.pose.position.x = point_3d[0]
         pose.pose.position.y = point_3d[1]
@@ -170,13 +203,17 @@ class Deprojection:
         
         # return pose
 
-    # this is the server function
-    def get_object_locations(self,request):
+    # this is the server function for the left camera
+    def left_get_object_locations(self,request):
         
         # save the current image as a source image
         # change the image from bgr to rgb
-        self.color_image = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(self.source_image_path, self.color_image)
+        color_image = cv2.cvtColor(self.left_color_image, cv2.COLOR_BGR2RGB)
+        depth_image = self.left_depth_image
+        camera_model = self.left_camera_model
+        camera_info = self.left_camera_info
+        
+        cv2.imwrite(self.source_image_path, color_image)
 
         # execute Grounding Dino here to get the bounding boxes based on the prompt
         image_path = self.source_image_path
@@ -194,7 +231,7 @@ class Deprojection:
         )
 
         annotated_frame = self.annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
-        cv2.imwrite("inference_images/annotated_image.jpg", annotated_frame)
+        cv2.imwrite("inference_images/annotated_image_left.jpg", annotated_frame)
 
         result = ObjectPositions()
 
@@ -229,7 +266,7 @@ class Deprojection:
             object_position.Class = phrases[i]
             x = int((xyxy[i][0] + xyxy[i][2]) /2)
             y = int((xyxy[i][1] + xyxy[i][3]) /2)
-            pose = self.get_3d_position(x, y)
+            pose = self.get_3d_position(x, y,depth_image=depth_image,camera_info=camera_info, camera_model=camera_model)
             if pose is None:
                 return
             object_position.pose = pose
@@ -244,19 +281,111 @@ class Deprojection:
 
         return GetObjectLocationsResponse(result)
     
-    def color_image_callback(self, msg: Image):
-        self.color_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    # this is the server function for the right camera
+    def right_get_object_locations(self,request):
+        
+        # save the current image as a source image
+        # change the image from bgr to rgb
+        if self.right_color_image is None :
+            rospy.logerr("Right image none")
+        color_image = cv2.cvtColor(self.right_color_image, cv2.COLOR_BGR2RGB)
+        depth_image = self.right_depth_image
+        camera_model = self.right_camera_model
+        camera_info = self.right_camera_info
+        
+        cv2.imwrite(self.source_image_path, color_image)
+
+        # execute Grounding Dino here to get the bounding boxes based on the prompt
+        image_path = self.source_image_path
+        text_prompt = TEXT_PROMPT
+        box_threshold = BOX_THRESHOLD
+        text_threshold = TEXT_THRESHOLD
+        image_source, image = load_image(image_path)
+
+        boxes, logits, phrases = predict(
+            model=self.model,
+            image=image,
+            caption=text_prompt,
+            box_threshold=box_threshold,
+            text_threshold=text_threshold
+        )
+
+        annotated_frame = self.annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
+        cv2.imwrite("inference_images/annotated_image_right.jpg", annotated_frame)
+
+        result = ObjectPositions()
+
+        h, w, _ = image_source.shape
+        boxes = boxes * torch.Tensor([w, h, w, h])
+        xyxy = box_convert(boxes=boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy().astype(int)
+        detections = sv.Detections(xyxy=xyxy)
+
+        labels = [
+            f"{phrase} {logit:.2f}"
+            for phrase, logit
+            in zip(phrases, logits)
+        ]
+
+        print("########################")
+        print("\nBoxes : ")
+        print(boxes, type(boxes))
+        print("\nBoxes : ")
+        print(xyxy, type(xyxy))
+        print("\nLabels : ")
+        print(labels, type(labels))
+        print("\nLabels : ")
+        print(phrases, type(phrases))
+        print("\n")
+        print("########################")
+
+
+        # PREPARE THE RESULT OBJECT HERE
+        for i in range(len(phrases)):
+            object_position = ObjectPosition()
+            object_position.id = i
+            object_position.Class = phrases[i]
+            x = int((xyxy[i][0] + xyxy[i][2]) /2)
+            y = int((xyxy[i][1] + xyxy[i][3]) /2)
+            pose = self.get_3d_position(x, y,depth_image=depth_image,camera_info=camera_info, camera_model=camera_model)
+            if pose is None:
+                return
+            object_position.pose = pose
+            object_position.x_min = xyxy[i][0]
+            object_position.y_min = xyxy[i][1]
+            object_position.x_max = xyxy[i][2]
+            object_position.y_max = xyxy[i][3]
+            result.object_position.append(object_position)
+
+        result.image = self.cv_bridge.cv2_to_imgmsg(annotated_frame, encoding="bgr8")
+        self.latest_result = result
+
+        return GetObjectLocationsResponse(result)
+
+    def left_color_image_callback(self, msg: Image):
+        self.left_color_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
         pass
 
-    def depth_image_callback(self, msg: Image):
-        self.depth_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    def left_depth_image_callback(self, msg: Image):
+        self.left_depth_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
         pass
     
-    def camera_info_callback(self, msg: CameraInfo):
-        self.camera_info = msg
-        self.camera_model.fromCameraInfo(msg)
+    def left_camera_info_callback(self, msg: CameraInfo):
+        self.left_camera_info = msg
+        self.left_camera_model.fromCameraInfo(msg)
         pass
 
+    def right_color_image_callback(self, msg: Image):
+        self.right_color_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+        pass
+
+    def right_depth_image_callback(self, msg: Image):
+        self.right_depth_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+        pass
+    
+    def right_camera_info_callback(self, msg: CameraInfo):
+        self.right_camera_info = msg
+        self.right_camera_model.fromCameraInfo(msg)
+        pass
 
 if __name__ == "__main__":
     rospy.init_node("deprojection_node")
